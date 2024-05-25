afc77082257b4aeaad0d0e956913a3cc9b7d0e31
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-26 00:12:13 +0100

    resolve

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    files: ["*.js"],
+    ignores: ["babel.config.js"],
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: "module",
+    },
+    plugins: {
+      jest: require("eslint-plugin-jest"),
+    },
+    rules: {
+      "no-console": "off",
+      "no-shadow": "off",
+      "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
+    },
+  },
+  {
+    files: ["*.js"],
+    languageOptions: {
+      globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+      },
+    },
+    settings: {
+      jest: {
+        version: 26,
+      },
+    },
+  },
+  {
+    files: ["*.js"],
+    linterOptions: {
+      extends: ["airbnb-base", "plugin:jest/all"],
+    },
+    environment: {
+      browser: false,
+      es6: true,
+      jest: true,
+    },
+  },
+];
