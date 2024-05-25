2df5cb81d23edcece0a4257bc03d9d47ea931dd8
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-25 21:35:59 +0100

    Resolving conflict

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 /* jshint esversion: 6 */
-export default function getResponseFromAPI() {
-=======
 function getResponseFromAPI() {
->>>>>>> 222f376477c7b5af320ec1e7d23348d98ff9c566
     return new Promise(() => {});
   }
   module.exports = getResponseFromAPI;
