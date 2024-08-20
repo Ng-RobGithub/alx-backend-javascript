cacf1d2160ad788ab90e713abc5d88d80c1f0b65
Ng-RobGithub <ngrob4real@gmail.com>
2024-08-20 21:05:08 +0100

    creating a small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+});
+
+app.use((req, res) => {
+    res.status(404).send('Cannot GET ' + req.originalUrl);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+module.exports = app;
