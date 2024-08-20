85221c7fd2590e8290cfa1dafbf32633ab56448b
Ng-RobGithub <ngrob4real@gmail.com>
2024-08-20 16:06:39 +0100

    1-stdin.js that will be executed through command line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,12 @@
-// Import the 'readline' module to handle input from stdin
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create an interface for input and output streams
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-// Prompt the user for their name
-console.log('Welcome to Holberton School, what is your name?');
-
-// Listen for user input
-rl.question('', (input) => {
-  // Output the user's name
-  console.log(`Your name is: ${input}`);
-  
-  // Close the readline interface and output the closing message
-  rl.close();
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
